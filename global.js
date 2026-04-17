308526a2d3ce26255a48f626e7e711689d7aff94
Ricki (Rui) Chen <chenrui.razx@outlook.com>
2026-04-16 21:38:11 -0700

    add automatical nav
    add auto nav

--- global.js
+++ global.js
@@ -18,3 +18,20 @@ console.log(currentLink);
 // add the current class to the matched nav link
 currentLink?.classList.add("current");
 
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/ricki-c', title: 'Profile' },
+  { url: 'resume/', title: 'Resume' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
