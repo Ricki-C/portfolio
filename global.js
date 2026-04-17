86f630db6b4c47c09f10ab37c48a6157589332f0
Ricki (Rui) Chen <chenrui.razx@outlook.com>
2026-04-17 12:52:17 -0700

    Update global.js
    s

--- global.js
+++ global.js
@@ -39,17 +39,3 @@ for (let p of pages) {
 
   nav.append(a);
 }
-
-// select all links inside nav
-let navLinks = $$("nav a");
-console.log(navLinks);
-
-// find the nav link that matches the current page URL
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
-
-console.log(currentLink);
-
-// add the current class to the matched nav link
-currentLink?.classList.add("current");
