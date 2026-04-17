a7b61798d407478bfcf2e18cc5a0a4414380b6c1
Ricki (Rui) Chen <chenrui.razx@outlook.com>
2026-04-16 20:35:54 -0700

    Update global.js
    automate the highlight

--- global.js
+++ global.js
@@ -3,3 +3,10 @@ console.log("IT’S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// select all links inside nav
+let navLinks = $$("nav a");
+console.log(navLinks);
+
+// print them in console for checking
+console.log(navLinks);
