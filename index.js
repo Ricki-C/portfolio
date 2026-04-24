6d9380bede9c2e60d957d241869f275b1e969123
Ricki (Rui) Chen <chenrui.razx@outlook.com>
2026-04-23 22:43:02 -0700

    bar style

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const profileStats = document.querySelector('#profile-stats');
 
 if (profileStats) {
   profileStats.innerHTML = `
+    <h2>My GitHub Stats</h2>
     <dl>
-      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-      <dt>Followers:</dt><dd>${githubData.followers}</dd>
-      <dt>Following:</dt><dd>${githubData.following}</dd>
+      <dt>Followers</dt>
+      <dd>${githubData.followers}</dd>
+
+      <dt>Following</dt>
+      <dd>${githubData.following}</dd>
+
+      <dt>Public Repos</dt>
+      <dd>${githubData.public_repos}</dd>
+
+      <dt>Public Gists</dt>
+      <dd>${githubData.public_gists}</dd>
     </dl>
   `;
 }
