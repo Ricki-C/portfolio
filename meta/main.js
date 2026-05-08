6b81832a35d82993fae50e98f4f55e0616907d58
Ricki (Rui) Chen <chenrui.razx@outlook.com>
2026-05-08 16:58:08 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -124,6 +124,13 @@ function renderScatterPlot(data, commits) {
     .attr('transform', `translate(${usableArea.left}, 0)`)
     .call(yAxis);
 
+    const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+
+    const rScale = d3
+    .scaleLinear()
+    .domain([minLines, maxLines])
+    .range([2, 30]);
+
   const dots = svg.append('g').attr('class', 'dots');
 
     dots
@@ -132,9 +139,12 @@ function renderScatterPlot(data, commits) {
     .join('circle')
     .attr('cx', (d) => xScale(d.datetime))
     .attr('cy', (d) => yScale(d.hourFrac))
-    .attr('r', 5)
+    .attr('r', (d) => rScale(d.totalLines))
     .attr('fill', 'steelblue')
+    .style('fill-opacity', 0.7)
     .on('mouseenter', (event, commit) => {
+        d3.select(event.currentTarget).style('fill-opacity', 1);
+
         renderTooltipContent(commit);
         updateTooltipVisibility(true);
         updateTooltipPosition(event);
@@ -142,12 +152,10 @@ function renderScatterPlot(data, commits) {
     .on('mousemove', (event) => {
         updateTooltipPosition(event);
     })
-    .on('mouseleave', () => {
-        updateTooltipVisibility(false);
-    })
+    .on('mouseleave', (event) => {
+        d3.select(event.currentTarget).style('fill-opacity', 0.7);
 
-    .on('mousemove', (event) => {
-    updateTooltipPosition(event);
+        updateTooltipVisibility(false);
     });
 }
 
